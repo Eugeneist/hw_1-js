0d1a3b8072227c30bd1f8b7053b468fe174c1577
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-02-02 09:03:26 +0200

    created index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// ЗАДАЧА 1
+
+let userAge = prompt("Привет! Сколько тебе лет?");
+
+alert(`Тебе ${userAge} лет! Проходи!`);
+
+
+// ЗАДАЧА 2
+
+let a = 3;
+let b = 6;
+
+alert( a + b );
+alert( b - a );
+alert( a * b );
+alert( b / a );
+alert( b ** a );
